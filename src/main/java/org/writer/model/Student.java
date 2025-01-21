81bf12666817a4acfb6fb845313f9653a6a853e4
Daniil <astafiev.den@gmail.com>
2025-01-21 18:44:04 +0300

    added annotation to classes

--- src/main/java/org/writer/model/Student.java
+++ src/main/java/org/writer/model/Student.java
@@ -3,12 +3,14 @@ package org.writer.model;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import org.writer.CSV;
 
 import java.util.List;
 
 @Data
 @Builder
 @AllArgsConstructor
+@CSV
 public class Student {
 
     private String name;
