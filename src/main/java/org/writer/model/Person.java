c6197d10a595196a957d1a488392ff33e0d581a3
posy9 <dannildtannk@gmail.com>
2025-06-05 14:56:52 +0300

    add Writer шimplemintation with JavaDoc

--- src/main/java/org/writer/model/Person.java
+++ src/main/java/org/writer/model/Person.java
@@ -3,20 +3,26 @@ package org.writer.model;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import org.writer.CsvField;
 
 @Data
 @Builder
 @AllArgsConstructor
 public class Person {
 
+    @CsvField(name = "FirstName")
     private String firstName;
 
+    @CsvField(name = "LastName")
     private String lastName;
 
+    @CsvField(name = "DayOfBirth")
     private int dayOfBirth;
 
+    @CsvField(name = "MonthOfBirth")
     private Months monthOfBirth;
 
+    @CsvField
     private int yearOfBirth;
 
 }
