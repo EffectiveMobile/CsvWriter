df735effda811b86425f1100e2cbb5a0c699b1ef
Andrey Nosov <107930557+swankygawk@users.noreply.github.com>
2025-09-29 16:28:38 +0200

    feat: add usage examples

--- src/main/java/org/writer/model/Student.java
+++ src/main/java/org/writer/model/Student.java
@@ -3,15 +3,19 @@ package org.writer.model;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import org.writer.annotation.CsvColumn;
+import org.writer.annotation.CsvEntity;
 
 import java.util.List;
 
 @Data
 @Builder
 @AllArgsConstructor
+@CsvEntity
 public class Student {
-
+    @CsvColumn(name = "name", order = 1)
     private String name;
 
+    @CsvColumn(name = "score", order = 2)
     private List<String> score;
 }
