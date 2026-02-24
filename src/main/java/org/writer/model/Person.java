bdac94ae94b9cff7cc2267bc87e02638b5a38ac5
ZhelnovachevRoman <zhelnovachev@gmail.com>
2026-02-24 22:46:39 +0300

    Написал: - необходимую реализацию Writable; - написал unit тесты и оформил JavaDoc для сервиса и утилитного класса; - использовал Datafaker для генерации студентов и персон

--- src/main/java/org/writer/model/Person.java
+++ src/main/java/org/writer/model/Person.java
@@ -3,20 +3,26 @@ package org.writer.model;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import org.writer.annotation.ColumnCsv;
 
 @Data
 @Builder
 @AllArgsConstructor
 public class Person {
 
+    @ColumnCsv(name = "имя")
     private String firstName;
 
+    @ColumnCsv(name = "фамилия")
     private String lastName;
 
+    @ColumnCsv(name = "день_рождения")
     private int dayOfBirth;
 
+    @ColumnCsv(name = "месяц_рождения")
     private Months monthOfBirth;
 
+    @ColumnCsv(name = "год_рождения")
     private int yearOfBirth;
 
 }
